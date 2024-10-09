d05d6b6f2bc4f7e1fd429189348af8a87a89c210
Reviver <hero_kong@live.com>
2024-10-09 14:21:21 +0700

    pc

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,46 +1,40 @@
-// import React, { useState } from "react";
-// import "../Style/main.css";
+import React, { useState } from "react";
+import "../Style/main.css";
 
-// // const logo = "https://fs.cdnxn.com/landingAi/Images/logo.png";
-// const headerCoin = "https://fs.cdnxn.com/landingAi/Images/icon-coid.png";
-// const headerNofi = "https://fs.cdnxn.com/landingAi/Images/icon-nofi.png";
-// const headerBurger = "https://fs.cdnxn.com/landingAi/Images/icon-burger.png";
-// const headerLine = "https://fs.cdnxn.com/landingAi/Images/icon-line.png";
+// img
+import logo from "../Images/new-ai/logo.png";
 
-// const login = "https://fs.cdnxn.com/landingAi/Images/btn-login-pc.png";
-// const register = "https://fs.cdnxn.com/landingAi/Images/btn-register-pc.png";
+export default function Header() {
+    const [linkLogin, setLinkLogin] = useState("https://game.lsm99ai.asia/login");
+    const [linkRegister, setLinkRegister] = useState("https://game.lsm99ai.asia/register");
+    const [linkHome, setLinkHome] = useState("/");
+    return (
+        <>
+            <div class="header-wrapper">
+                <div className="head-group">
+                    <a href={linkLogin}>
+                        <div className="head-login">
+                            <span>เข้าสู่ระบบ</span>
+                        </div>
+                    </a>
 
-// export default function Header() {
-//   const [linkLogin, setLinkLogin] = useState("https://game.lsm99ai.asia/login");
-//   const [linkRegister, setLinkRegister] = useState("https://game.lsm99ai.asia/register");
-//   const [linkHome, setLinkHome] = useState("/");
-//   return (
-//     <>
-//       <div class="header-wrapper">
-//         <div className="header">
-//           <div class="left">
-//             <div className="pc">
-//               <div className="login-register">
-//                 <a href="https://game.lsm99ai.bet/login"><img src={login} alt="login" /></a>
-//                 <a href="https://game.lsm99ai.bet/register"><img src={register} alt="register" /></a>
-//               </div>
-//             </div>
-//           </div>
-//           <a href="/">
-//             <div class="center">
-//               <div className="pc">
-//               </div>
-//             </div>
-//           </a>
-//           <div class="right">
-//             <div className="container-right">
-//               <div className="line-pc"><a href="https://lin.ee/izXwWe4" target="_blank"><img src={headerLine} alt="line" className="pc" /></a></div>
-//               <div className="notification"><a href={linkLogin}><img src={headerNofi} alt="notification" /></a></div>
-//               <div className="burger"><a href={linkLogin}><img src={headerBurger} alt="hamburger bar" /></a></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+                    <div className="head-logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <a href={linkRegister}>
+                        <div className="head-register">สมัครสมาชิก</div>
+                    </a>
+
+                </div>
+                <div className="head-grou-mb">
+                    <a href={linkLogin}>
+                        <div className="head-login-mb">เข้าสู่ระบบ</div>
+                    </a>
+                    <a href={linkRegister}>
+                        <div className="head-register-mb">สมัครสมาชิก</div>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,6 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 
 import Header from "../Pages/Header";
-import Footer from "../Pages/Footer";
 
 // img
 import logostart from "../Images/new-ai/logo-start.png";
@@ -21,6 +20,46 @@ import BannerPro2 from "../Images/new-ai/Promotion/Pro2.png";
 import BannerPro3 from "../Images/new-ai/Promotion/Pro3.png";
 import BannerPro4 from "../Images/new-ai/Promotion/Pro4.png";
 
+import iconGames1 from "../Images/new-ai/icon-games/s1.png";
+import iconGames2 from "../Images/new-ai/icon-games/s2.png";
+import iconGames3 from "../Images/new-ai/icon-games/s3.png";
+import iconGames4 from "../Images/new-ai/icon-games/s4.png";
+import iconGames5 from "../Images/new-ai/icon-games/s5.png";
+import iconGames6 from "../Images/new-ai/icon-games/s6.png";
+import iconGames7 from "../Images/new-ai/icon-games/s7.png";
+import iconGames8 from "../Images/new-ai/icon-games/s8.png";
+
+import iconGamec1 from "../Images/new-ai/icon-games/c1.png";
+import iconGamec2 from "../Images/new-ai/icon-games/c2.png";
+import iconGamec3 from "../Images/new-ai/icon-games/c3.png";
+import iconGamec4 from "../Images/new-ai/icon-games/c4.png";
+import iconGamec5 from "../Images/new-ai/icon-games/c5.png";
+import iconGamec6 from "../Images/new-ai/icon-games/c6.png";
+import iconGamec7 from "../Images/new-ai/icon-games/c7.png";
+import iconGamec8 from "../Images/new-ai/icon-games/c8.png";
+
+import iconGamesp1 from "../Images/new-ai/icon-games/sp1.png";
+import iconGamesp2 from "../Images/new-ai/icon-games/sp2.png";
+import iconGamesp3 from "../Images/new-ai/icon-games/sp3.png";
+import iconGamesp4 from "../Images/new-ai/icon-games/sp4.png";
+import iconGamesp5 from "../Images/new-ai/icon-games/sp5.png";
+
+import iconGamel1 from "../Images/new-ai/icon-games/l1.png";
+import iconGamel2 from "../Images/new-ai/icon-games/l2.png";
+import iconGamel3 from "../Images/new-ai/icon-games/l3.png";
+
+import bannerquestion from "../Images/new-ai/img-question.png";
+
+import footerlogo from "../Images/new-ai/logo.png";
+
+import iconBank1 from "../Images/new-ai/icon-bank/b1.png";
+import iconBank2 from "../Images/new-ai/icon-bank/b2.png";
+import iconBank3 from "../Images/new-ai/icon-bank/b3.png";
+import iconBank4 from "../Images/new-ai/icon-bank/b4.png";
+import iconBank5 from "../Images/new-ai/icon-bank/b5.png";
+import iconBank6 from "../Images/new-ai/icon-bank/b6.png";
+import iconBank7 from "../Images/new-ai/icon-bank/b7.png";
+
 export default function Home() {
   const [linkLogin, setLinkLogin] = useState("https://game.lsm99ai.asia/login");
   const [linkRegister, setLinkRegister] = useState(
@@ -33,31 +72,31 @@ export default function Home() {
 
   const [currentBanner, setCurrentBanner] = useState(0);
 
-  // const gameImages = {
-  //   slot: [iconGames1, iconGames2, iconGames3, iconGames4, iconGames5, iconGames6, iconGames7, iconGames8],
-  //   lotto: [iconGamel1, iconGamel2, iconGamel3],
-  //   casino: [iconGamec1, iconGamec2, iconGamec3, iconGamec4, iconGamec5, iconGamec6, iconGamec7, iconGamec8],
-  //   sport: [iconGamesp1, iconGamesp2, iconGamesp3, iconGamesp4, iconGamesp5],
-  // };
+  const gameImages = {
+    slot: [iconGames1, iconGames2, iconGames3, iconGames4, iconGames5, iconGames6, iconGames7, iconGames8],
+    lotto: [iconGamel1, iconGamel2, iconGamel3],
+    casino: [iconGamec1, iconGamec2, iconGamec3, iconGamec4, iconGamec5, iconGamec6, iconGamec7, iconGamec8],
+    sport: [iconGamesp1, iconGamesp2, iconGamesp3, iconGamesp4, iconGamesp5],
+  };
 
   // Resize effect to control displayed games
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     if (window.innerWidth < 1100) {
-  //       setDisplayedGames(gameImages[selectedCategory].slice(0, 6));
-  //     } else {
-  //       setDisplayedGames(gameImages[selectedCategory]);
-  //     }
-  //   };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1100) {
+        setDisplayedGames(gameImages[selectedCategory].slice(0, 6));
+      } else {
+        setDisplayedGames(gameImages[selectedCategory]);
+      }
+    };
 
-  //   handleResize();
+    handleResize();
 
-  //   window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
 
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, [selectedCategory]);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [selectedCategory]);
 
   // Auto switch categories
   useEffect(() => {
@@ -146,9 +185,10 @@ export default function Home() {
   return (
     <>
       <div>
+        <Header />
         <div className="group-container">
           <div className="section-start container-box">
-            <div className="flex-container">
+            <div className="flex-section-start">
               <div className="section-start-text">
                 <div className="img-start-logo">
                   <img src={logostart} alt="logo" />
@@ -168,6 +208,34 @@ export default function Home() {
               <img src={titlemenu2} alt="menu" />
               <img src={titlemenu3} alt="menu" />
             </div>
+            <Splide
+              className="section-menu-custom-slider"
+              options={{
+                rewind: true,
+                gap: "10px",
+                type: "loop",
+                autoplay: true,
+                interval: 3000,
+                arrows: false,
+                pauseOnHover: false,
+                perPage: 1,
+                focus: 'center',
+                padding: {
+                  left: '20%',
+                  right: '20%'
+                }
+              }}
+            >
+              <SplideSlide className="section-menu-custom-slider">
+                <img src={titlemenu1} alt="" className="w-100" />
+              </SplideSlide>
+              <SplideSlide className="section-menu-custom-slider">
+                <img src={titlemenu2} alt="" className="w-100" />
+              </SplideSlide>
+              <SplideSlide className="section-menu-custom-slider">
+                <img src={titlemenu3} alt="" className="w-100" />
+              </SplideSlide>
+            </Splide>
           </div>
           <div className="section-title">
             <div>
@@ -176,8 +244,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="section-promotion">
-            <div className="promotion-box">
+          <div className="section-promotion container-box">
+            <div className="flex-container">
               <div>
                 <div className="promotion-banner">
                   <Splide
@@ -259,8 +327,75 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="section-games"></div>
-          <div className="section-question"></div>
+          <div className="section-games container-box">
+            <div>
+              <div className="games-box">
+                <div className="games-category">
+                  {gameCategories.map((category) => (
+                    <div
+                      key={category}
+                      className={`category ${selectedCategory === category ? 'active' : ''}`}
+                      onClick={() => setSelectedCategory(category)}
+                    >
+                      <div className={`category-${category}`}></div>
+                      <span>{category === 'slot' ? 'สล็อต' : category === 'lotto' ? 'หวย' : category === 'casino' ? 'คาสิโน' : 'กีฬา'}</span>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="games-list">
+                  {displayedGames.map((image, index) => (
+                    <a href={linkLogin} key={index}>
+                      <div className="icon-games">
+                        <img src={image} alt={selectedCategory} />
+                      </div>
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="section-question container-box">
+            <div className="flex-container">
+              <div className="section-question-banner">
+                <img src={bannerquestion} alt="banner" />
+              </div>
+              <div className="section-question-text">
+                <p>ทำไมคุณควรตัดสินใจเลือกเรา ?<br /><br />
+
+                  เว็บคาสิโนออนไลน์ที่ครองใจผู้เล่นทั่วโลก ไม่ใช่แค่ประเทศจีน
+                  ด้วยมาตรฐานระดับโลก เทคโนโลยี และการบริการดีที่สุด
+                  ไร้ปัญหา ให้บริการครบทั้ง dg บาคาร่าออนไลน์ ไฮโล เสือมังกร
+                  รูเล็ต หรือ โป๊กเกอร์ บริษัทพัฒนาต่อเนื่องรวดเร็ว มั่นคง
+                  พร้อมการ ใช้งาน ผ่าน Casino App ยิ่งตอกย้ำ ความเป็นเจ้า
+                  ตลาดของ เกมคาสิโน ภาพ Full HD ได้ประสบการณ์พร้อมกับ
+                  สนุกทุกที่และลุ้นโชค ไปกับเกมส์ของเรา
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="section-footer">
+            <div className="section-footer-group">
+              <div className="footerlogo">
+                <img src={footerlogo} alt="logo" />
+              </div>
+              <div className="footer-text">
+                <span>
+                  LSM99Ai ศูนย์รวมเว็บพนันออนไลน์ทุกชนิด ฝาก-ถอนด้วยระบบ<br />
+                  ออโต้ทำรายการไม่กี่วินาที พร้อมรูปแบบการเล่นง่าย สมัครง่ายในไม่กี่ขั้น ตอนบริการด้วยระบบออนไลน์มาตรฐานสากล เท่าไหร่ก็จ่าย
+                </span>
+              </div>
+              <div className="icon-bank">
+                <img src={iconBank1} alt="icon-bank" />
+                <img src={iconBank2} alt="icon-bank" />
+                <img src={iconBank3} alt="icon-bank" />
+                <img src={iconBank4} alt="icon-bank" />
+                <img src={iconBank5} alt="icon-bank" />
+                <img src={iconBank6} alt="icon-bank" />
+                <img src={iconBank7} alt="icon-bank" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
